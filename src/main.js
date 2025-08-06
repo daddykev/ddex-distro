79670bdd120da7e3d600aaa85346cc5da986e3bf
Kevin Moo <kevinmoo@Kevins-MacBook-Pro.local>
2025-08-06 14:24:03 -0700

    FontAwesome icons deployed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,42 @@ import router from './router'
 // Import the main CSS file (which imports all other CSS files)
 import './assets/main.css'
 
+// FontAwesome imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Import specific icons from free solid icons
+import { 
+  faHome,
+  faMusic,
+  faUpload,
+  faPlus,
+  faEdit,
+  faTrash,
+  faCheck,
+  faTimes,
+  faCog,
+  faChartBar
+} from '@fortawesome/free-solid-svg-icons'
+
+// Add icons to the library
+library.add(
+  faHome,
+  faMusic,
+  faUpload,
+  faPlus,
+  faEdit,
+  faTrash,
+  faCheck,
+  faTimes,
+  faCog,
+  faChartBar
+)
+
 const app = createApp(App)
+
+// Register FontAwesome component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
